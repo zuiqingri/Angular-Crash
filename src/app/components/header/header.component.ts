41e9f3bddcffc27ae4529a7878d878c07b01124a
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-12-13 21:55:56 +0800

    UiService-toggleShow

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title = 'Task Tracker';
-  constructor() { }
+  showAddTask:boolean=false;
+  subscription:Subscription;
+  constructor(private uiService:UiService) { 
+  this.subscription=this.uiService.onToggle().subscribe((value)=>(this.showAddTask=value));
+  }
 
   ngOnInit(): void {
   }
   toggleAddTask(){
-    console.log('toggle')
+    this.uiService.toggleAddTask();
   }
 }
